2b4bb32e659e9317076893ec08d4c59fe9cee71d
Abhat27 <advaithbhat9@gmail.com>
2019-07-03 14:01:56 +0000

    Nuke Klapse

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -59,12 +59,6 @@
 #include "mdp3_ctrl.h"
 
 
-#include "mdss_livedisplay.h"
-
-#ifdef CONFIG_KLAPSE
-#include <linux/klapse.h>
-#endif
-
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
 #else
